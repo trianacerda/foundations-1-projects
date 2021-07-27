85cb8a000620883525d240bda36e3b7ed52a0be5
Triana Cerda <trianaRcerda@gmail.com>
2021-07-27 11:08:41 -0700

    refactored artist + song titling + syntax clean up

--- music-data.js
+++ music-data.js
@@ -2,64 +2,64 @@ const PLAYLIST = [
 
     {
         artist:'Redbone',
-        title:`Come and Get Your Love`,
-        path:`Redbone - Come and Get Your Love.mp3`
+        title:`Come And Get Your Love`,
+        path:`Redbone - Come And Get Your Love.mp3`
 
     },
     
-    // {
-    //     artist:'Looking Glass',
-    //     title:`Brandy (You're a Fine Girl)`,
-    //     path:`${artist} - ${title}.mp3 `
-    // },
+    {
+        artist:'Looking Glass',
+        title:`Brandy (You're a Fine Girl)`,
+        path:`Looking Glass - Brandy (You're a Fine Girl).mp3 `
+    },
 
-    // {
-    //     artist:'John Denver',
-    //     title:`Looking For Space`,
-    //     path:`${artist} - ${title}.mp3 `
-    // }, 
+    {
+        artist:'John Denver',
+        title:`Looking For Space`,
+        path:`John Denver - Looking For Space.mp3 `
+    }, 
 
-    // {
-    //     artist:'Loggins And Messina',
-    //     title:`Watching The River Run`,
-    //     path:`${artist} - ${title}.mp3 `
-    // },
+    {
+        artist:'Loggins And Messina',
+        title:`Watching The River Run`,
+        path:`Loggins And Messina - Watching the River Run.mp3 `
+    },
 
-    // {
-    //     artist:'Hall and Oates',
-    //     title:`Rich Girl`,
-    //     path:`${artist} - ${title}.mp3 `
-    // },
-    // {
-    //     artist:'Georgia Satellites',
-    //     title:`Keep your hands to yourself`,
-    //     path:`${artist} - ${title}.mp3 `
+    {
+        artist:'Hall And Oates',
+        title:`Rich Girl`,
+        path:`Hall And Oates - Rich Girl.mp3 `
+    },
+    {
+        artist:'Georgia Satellites',
+        title:`Keep Your Hands To Yourself`,
+        path:`Georgia Satellites - Keep Your Hand To Yourself.mp3 `
 
-    // },
+    },
     
-    // {
-    //     artist:'Don McLean',
-    //     title:`American Pie`,
-    //     path:`${artist} - ${title}.mp3 `
-    // },
+    {
+        artist:'Don McLean',
+        title:`American Pie`,
+        path:`Don McLean - American Pie .mp3 `
+    },
 
-    // {
-    //     artist:'America',
-    //     title:`Sister Golden Hair`,
-    //     path:`${artist} - ${title}.mp3 `
-    // }, 
+    {
+        artist:'America',
+        title:`Sister Golden Hair`,
+        path:`America - Sister Golden Hair}.mp3 `
+    }, 
 
-    // {
-    //     artist:'Loggins and Messina',
-    //     title:`Danny's Song`,
-    //     path:`${artist} - ${title}.mp3 `
-    // },
+    {
+        artist:'Loggins And Messina',
+        title:`Danny's Song`,
+        path:`Loggins And Messina - Danny's Song.mp3 `
+    },
 
-    // {
-    //     artist:'America',
-    //     title:`A Horse with No Name`,
-    //     path:`${artist} - ${title}.mp3 `
-    // }
+    {
+        artist:'America',
+        title:`A Horse With No Name`,
+        path:`America - A Horse With No Name.mp3 `
+    }
 
 
 ]
