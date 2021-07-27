767ea41c9e541175869083a2cc11456f7ef64781
Triana Cerda <trianaRcerda@gmail.com>
2021-07-27 15:43:05 -0700

    refactored music-data + added = to previousTrack function

--- music-data.js
+++ music-data.js
@@ -3,62 +3,62 @@ const PLAYLIST = [
     {
         artist:'Redbone',
         title:'Come And Get Your Love',
-        path:'Redbone-Come-and-Get-Your-Love.mp3'
+        path:'redbone-come-and-get-your-love.mp3'
 
     },
     
     {
         artist:'Looking Glass',
         title:`Brandy (You're a Fine Girl)`,
-        path:`Looking Glass - Brandy (You're a Fine Girl).mp3 `
+        path:`looking-glass-brandy-youre-a-fine-girl.mp3`
     },
 
     {
         artist:'John Denver',
         title:`Looking For Space`,
-        path:`John Denver - Looking For Space.mp3 `
+        path:`john-denver-looking-for-space.mp3`
     }, 
 
     {
         artist:'Loggins And Messina',
         title:`Watching The River Run`,
-        path:`Loggins And Messina - Watching the River Run.mp3 `
+        path:`loggins-and-messina-watching-the-river-run.mp3`
     },
 
     {
         artist:'Hall And Oates',
         title:`Rich Girl`,
-        path:`Hall And Oates - Rich Girl.mp3 `
+        path:`hall-and-oates-rich-girl.mp3`
     },
     {
         artist:'Georgia Satellites',
         title:`Keep Your Hands To Yourself`,
-        path:`Georgia Satellites - Keep Your Hand To Yourself.mp3 `
+        path:`georgia-satellites-keep-your-hands-to-yourself.mp3`
 
     },
     
     {
         artist:'Don McLean',
         title:`American Pie`,
-        path:`Don McLean - American Pie .mp3 `
+        path:`don-mclean-american-pie.mp3`
     },
 
     {
         artist:'America',
         title:`Sister Golden Hair`,
-        path:`America - Sister Golden Hair}.mp3 `
+        path:`america-sister-golden-hair.mp3`
     }, 
 
     {
         artist:'Loggins And Messina',
         title:`Danny's Song`,
-        path:`Loggins And Messina - Danny's Song.mp3 `
+        path:`loggins-and-messina-dannys-song.mp3`
     },
 
     {
         artist:'America',
         title:`A Horse With No Name`,
-        path:`America - A Horse With No Name.mp3 `
+        path:`america-a-horse-with-no-name.mp3`
     }
 
 
