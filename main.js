e8d6740fd2a421b09a08a213413a4a9e4c019f30
Alice-Retz <alicegretz@gmail.com>
2021-07-27 15:22:56 -0500

    Troubleshooting play/pause in actual web page

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const nextBtn = document.getElementById('next-track');
 let current_song = 0;
 let trackPlaying = false;
 
-setTrack(current_song);
 
 playBtn.addEventListener('click', () => {
     if (trackPlaying === false) {
@@ -21,14 +20,16 @@ playBtn.addEventListener('click', () => {
 let current_path = "";
 
 function setTrack(song_index){
-
+    
     current_path = './assets/songs/' + `${PLAYLIST[song_index].path}`;
-
+    
     musicPlayer.src = current_path;
-
+    
     // playTrack();
-
+    
 }
+
+setTrack(current_song);
 
 console.log(current_path);
 
