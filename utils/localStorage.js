e6cd0088067f2c48a46735b170d0731123a4e53a
Evon Perez <perez.evon@gmail.com>
2021-07-28 16:36:38 -0400

    added local storage fucntions, modified playlist data structure, removed console logs

---
+++ utils/localStorage.js
@@ -0,0 +1,29 @@
+import { shuffle } from "./utils.js";
+import PLAYLIST from "../music-data.js";
+import SCHEMES from "../color-scheme-data.js";
+const SETTINGS = 'Settings';
+
+const defaultScheme = SCHEME[0]; 
+const defaultPlaylist = PLAYLIST[0];
+
+
+export function getSettings(){
+
+let settings = localStorage.getItem( SETTINGS || '{}' );
+let parsedSettings = JSON.parse(settings);
+
+return parsedSettings;
+
+}
+
+export function setSettings( shuffledEnabled = false, selected_playlist = defaultPlaylist, selected_scheme = defaultScheme){
+
+let settings = {
+    playlist: selected_playlist,
+    scheme: selected_scheme,
+    shuffle: shuffledEnabled
+};
+
+localStorage.setItem(SETTINGS, JSON.stringify(settings));
+
+}
