4f738010232a66de7199d0612b175efb34dcc6ee
Alice-Retz <alicegretz@gmail.com>
2021-07-29 15:27:56 -0500

    Play/pause button image toggle fixed

--- settings/settings.js
+++ settings/settings.js
@@ -1,67 +1,76 @@
-export function changeColorSchemeToBnw() {
+export function changeColorSchemeToBnw(str) {
     let prevBtn = document.getElementById('previous-track');
     prevBtn.innerHTML = `<img src='./assets/buttons/replay-BnW.png' alt="previous-track">`;
 
     let nextBtn = document.getElementById('next-track');
     nextBtn.innerHTML = `<img src='./assets/buttons/skip-BnW.png' alt="next-track">`;
-    
+    if (str === 'pause') {
         let pauseBtn = document.getElementById('play-pause');
         pauseBtn.innerHTML = `<img src='./assets/buttons/pause-BnW.png' alt="pause-button">`;
-
-    let playBtn = document.getElementById('play-pause');
-    playBtn.innerHTML = `<img src='./assets/buttons/play-BnW.png' alt="play-button">`;
-
+    }
+    if (str === 'play') {
+        let playBtn = document.getElementById('play-pause');
+        playBtn.innerHTML = `<img src='./assets/buttons/play-BnW.png' alt="play-button">`;
+    }
     let body = document.getElementsByTagName('body');
     console.log(body);
     body[0].style.backgroundColor = '#000';
     }
     
-export function changeColorSchemeToPastel(){
+export function changeColorSchemeToPastel(str){
     let prevBtn = document.getElementById('previous-track');
     prevBtn.innerHTML = `<img src='./assets/buttons/replay-pastel.png' alt="previous-track">`;
     
     let nextBtn = document.getElementById('next-track');
     nextBtn.innerHTML = `<img src='./assets/buttons/skip-pastel.png' alt="next-track">`;
     
-    let pauseBtn = document.getElementById('play-pause');
-    pauseBtn.innerHTML = `<img src='./assets/buttons/pause-pastel.png' alt="pause-button">`;
-    
-    let playBtn = document.getElementById('play-pause');
-    playBtn.innerHTML = `<img src='./assets/buttons/play-pastel.png' alt="play-button">`;
-
+    if (str === 'pause') {
+        let pauseBtn = document.getElementById('play-pause');
+        pauseBtn.innerHTML = `<img src='./assets/buttons/pause-pastel.png' alt="pause-button">`;
+    }
+    if (str === 'play') {
+        let playBtn = document.getElementById('play-pause');
+        playBtn.innerHTML = `<img src='./assets/buttons/play-pastel.png' alt="play-button">`;
+    }
     let body = document.getElementsByTagName('body');
     body[0].style.backgroundColor = '#a2d2ffff';
     }
+    
 
-export function changeColorSchemeToWashout(){
+export function changeColorSchemeToWashout(str){
     let prevBtn = document.getElementById('previous-track');
     prevBtn.innerHTML = `<img src='./assets/buttons/replay-washout.png' alt="previous-track">`;
     
     let nextBtn = document.getElementById('next-track');
     nextBtn.innerHTML = `<img src='./assets/buttons/skip-washout.png' alt="next-track">`;
     
-    let pauseBtn = document.getElementById('play-pause');
-    pauseBtn.innerHTML = `<img src='./assets/buttons/pause-washout.png' alt="pause-button">`;
-    
-    let playBtn = document.getElementById('play-pause');
-    playBtn.innerHTML = `<img src='./assets/buttons/play-washout.png' alt="play-button">`;
-
+    if (str === 'pause') {
+        let pauseBtn = document.getElementById('play-pause');
+        pauseBtn.innerHTML = `<img src='./assets/buttons/pause-washout.png' alt="pause-button">`;
+    }
+    if (str === 'play') {
+        let playBtn = document.getElementById('play-pause');
+        playBtn.innerHTML = `<img src='./assets/buttons/play-washout.png' alt="play-button">`;
+    }
     let body = document.getElementsByTagName('body');
     body[0].style.backgroundColor = '#3d405bff';
     }
    
-export function changeColorSchemeToDefault(){
+export function changeColorSchemeToDefault(str){
     let prevBtn = document.getElementById('previous-track');
     prevBtn.innerHTML = `<img src='./assets/buttons/replay.png' alt="previous-track">`;
     
     let nextBtn = document.getElementById('next-track');
     nextBtn.innerHTML = `<img src='./assets/buttons/skip.png' alt="next-track">`;
     
-    let pauseBtn = document.getElementById('play-pause');
-    pauseBtn.innerHTML = `<img src='./assets/buttons/pause.png' alt="pause-button">`;
-
-    let playBtn = document.getElementById('play-pause');
-    playBtn.innerHTML = `<img src='./assets/buttons/play.png' alt="play-button">`;
+    if (str === 'pause') {
+        let pauseBtn = document.getElementById('play-pause');
+        pauseBtn.innerHTML = `<img src='./assets/buttons/pause.png' alt="pause-button">`;
+    } 
+    if (str === 'play') {
+        let playBtn = document.getElementById('play-pause');
+        playBtn.innerHTML = `<img src='./assets/buttons/play.png' alt="play-button">`;
+    }
     
     let body = document.getElementsByTagName('body');
     body[0].style.backgroundColor = '#00baffff';
